7b0984e2cf9b1cea80df12e1b9e6008cc8ca89b1
Glomik <glomyura@gmail.com>
2016-08-12 12:19:27 +0300

    Add files via upload

---
+++ main.php
@@ -0,0 +1,57 @@
+﻿<?php
+/**
+ * Telegram Bot access token и URL.
+ */
+$access_token = 'ВАШ_ACCESS_TOKEN';
+$api = 'https://api.telegram.org/bot' . $access_token;
+
+/**
+ * Задаём основные переменные.
+ */
+$output = json_decode(file_get_contents('php://input'), TRUE);
+$chat_id = $output['message']['chat']['id'];
+$first_name = $output['message']['chat']['first_name'];
+$message = $output['message']['text'];
+
+$what ="sell";
+$valyuta = "btc_uah";
+$siteAddress="https://btc-trade.com.ua/api/trades/" . $what . "/" . $valyuta;
+
+/**
+ * Emoji для лучшего визуального оформления.
+ */
+$emoji = array(
+  'preload' => json_decode('"\uD83D\uDE03"'), // Улыбочка.
+  'weather' => array(
+    'clear' => json_decode('"\u2600"'), // Солнце.
+    'clouds' => json_decode('"\u2601"'), // Облака.
+    'rain' => json_decode('"\u2614"'), // Дождь.
+    'snow' => json_decode('"\u2744"'), // Снег.
+  ),
+);
+
+/**
+ * Получаем команды от пользователя.
+ */
+switch($message) {
+  case '/test':
+    // Отправляем приветственный текст.
+    $preload_text = 'Одну секунду, ' . $first_name . ' ' . $emoji['preload'] . ' Я уточняю для вас курс..';
+    sendMessage($chat_id, $preload_text);
+    // API key для OpenWeatherMap.
+    $kurs1 = json_decode(file_get_contents(siteAddress1), TRUE);
+     // Формирование ответа.
+    $kurs_text = 'Сейчас по паре' . $valuta  . " такие показатели: ";
+    // Отправка ответа пользователю Telegram.
+    sendMessage($chat_id, $kurs_text );
+    break;
+  default:
+    break;
+}
+
+/**
+ * Функция отправки сообщения sendMessage().
+ */
+function sendMessage($chat_id, $message) {
+  file_get_contents($GLOBALS['api'] . '/sendMessage?chat_id=' . $chat_id . '&text=' . urlencode($message));
+}
