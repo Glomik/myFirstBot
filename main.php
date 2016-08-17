274008b38343b19bc811f3db5aee0a1e854c8591
Glomik <glomyura@gmail.com>
2016-08-17 20:43:57 +0300

    Update main.php

--- main.php
+++ main.php
@@ -147,15 +147,17 @@ function sendMessage($chat_id, $message) {
 function sendPhoto($chat_id, $name) {
 //  file_get_contents($GLOBALS['api'] . '/sendPhoto?chat_id=' . $chat_id . '&photo="' . $photo_id . '"');
   $url = $GLOBALS['api'] . '/sendPhoto' ;
-  
+  imagedestroy (draw_game ($name,'example.png',3,2);
+  /*
   $post_fields = array( 'chat_id' => $chat_id,
-        'photo'     =>   draw_game ('field.png','example.png',3,2)
+        'photo'     =>   
   );
-  /*  работает
+  */
+  //  работает
   $post_fields = array( 'chat_id' => $chat_id,
         'photo'     =>  new CURLFile( $name)
   );
-  */
+  //
   /*
   $ch = curl_init(); 
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
